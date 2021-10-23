5dc6f653120fde25123e8043c0fe395f462c8959
Pedro Brügger <65832126+BruggerPedro@users.noreply.github.com>
2021-10-23 14:27:39 -0300

    Create Vinho.java

---
+++ ExInterfaceEAbstrataD/Vinho.java
@@ -0,0 +1,43 @@
+package ExInterfaceEAbstrataD;
+
+public class Vinho implements Produto {
+    private String id, categoria;
+    private int anoProducao, unidadeVenda;
+    private float preco;
+
+    public Vinho(String id, String categoria, int anoProducao, int unidadeVenda, float preco) {
+        this.id = id;
+        this.categoria = categoria;
+        this.anoProducao = anoProducao;
+        this.unidadeVenda = unidadeVenda;
+        this.preco = preco;
+    }
+
+    public String id() {
+        return this.id;
+    }
+
+    public int anoProducao() {
+        return this.anoProducao;
+    }
+
+    public int unidadeVenda() {
+        return this.unidadeVenda;
+    }
+
+    public String categoria() {
+        return this.categoria;
+    }
+
+    public float preco() {
+        return  this.preco;
+    }
+
+    public void mostraDados() {
+        System.out.println("Id: " + id());
+        System.out.println("Ano de produção: " + anoProducao());
+        System.out.println("Unidade de venda: " + unidadeVenda());
+        System.out.println("Categoria: " + categoria());
+        System.out.println("Preço: " + preco());
+    }
+}
