5c0c4db85831fd66b370ca73beb86069f8a7a498
Pedro Brügger <65832126+BruggerPedro@users.noreply.github.com>
2021-10-23 14:26:03 -0300

    Create Produto.java

---
+++ ExInterfaceEAbstrataD/Produto.java
@@ -0,0 +1,15 @@
+package ExInterfaceEAbstrataD;
+
+public interface Produto {
+    public String id();
+
+    public int anoProducao();
+
+    public int unidadeVenda();
+
+    public String categoria();
+
+    public float preco();
+
+    public void mostraDados();
+}
